7a6fdea0bdb2bed2973a460b2f1f7a8c79084d27
Kevin Bastian <kjb5642@rit.edu>
2017-10-18 00:25:05 -0400

    Adding Bulma for Vue and the Register component

---
+++ client/src/components/Register.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1> Register </h1>
+    <form>
+        <div class="field">
+            <label class="label">Email</label>
+            <div class="control has-icons-left has-icons-right">
+                <input class="input" type="email" placeholder="Email input" value="">
+                <span class="icon is-small is-left">
+                <i class="fa fa-envelope"></i>
+                </span>
+            </div>
+        </div>
+
+
+        <label for="password"> Password </label>
+        <input type="password" name="password" id="password">
+    </form>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/Register'
 
+// Use Bulma for Vue
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+
+Vue.use(Buefy)
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +16,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
